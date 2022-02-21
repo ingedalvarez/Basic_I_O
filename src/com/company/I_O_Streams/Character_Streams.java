8818d3ccaaeb05b5dcfb367a27e22b0a5056ff75
ingedalvarez <ingedalvarez@gmail.com>
2022-02-21 09:45:07 -0500

    Character Stream example ended

---
+++ src/com/company/I_O_Streams/Character_Streams.java
@@ -0,0 +1,22 @@
+package com.company.I_O_Streams;
+
+import java.io.*;
+
+public class Character_Streams {
+    public static void main(String[] args) {
+     int c;
+     try(FileReader in = new FileReader("xanadu.txt");
+         FileWriter out = new FileWriter("character-output.txt");){
+
+         while ((c = in.read())!= -1){
+             out.write(c);
+             System.out.format("%d ", c);
+         }
+     } catch (FileNotFoundException e) {
+         e.printStackTrace();
+     } catch (IOException e) {
+         e.printStackTrace();
+     }
+
+    }
+}
