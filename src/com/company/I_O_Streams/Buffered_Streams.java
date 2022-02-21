7388679a2fc4bbfd6461e19fa9e358759a7b2fbc
ingedalvarez <ingedalvarez@gmail.com>
2022-02-21 10:26:35 -0500

    Buffered Stream example ended

---
+++ src/com/company/I_O_Streams/Buffered_Streams.java
@@ -0,0 +1,24 @@
+package com.company.I_O_Streams;
+
+
+import java.io.*;
+
+public class Buffered_Streams {
+    public static void main(String[] args) throws IOException {
+        int c;
+
+
+        try(BufferedReader in = new BufferedReader(new FileReader("xanadu.txt"));
+            BufferedWriter out = new BufferedWriter(new FileWriter("character_output_bufferedReader.txt"));) {
+            while((c=in.read())!=-1){
+                out.write(c);
+                System.out.format("%d ", c);
+
+            }
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
